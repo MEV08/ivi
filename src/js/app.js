06bd3e981f88999eb20d069a3a301d3e60c529b5
Evgeniy <emashkovtsev@gmail.com>
2021-10-01 04:15:33 +0300

    some changes

--- src/js/app.js
+++ src/js/app.js
@@ -4,26 +4,30 @@ const start = document.querySelector('#start');
 const count = document.querySelector('#count');
 let isStarted = false;
 
-diamond.addEventListener('click', () => {
-    start.style.display = 'none';
-    count.style.display = 'block';
-    isStarted = true;
-    let time = 1;
-    count.textContent = time;
-    let timer = setInterval(() => {
-        time++;
+
+diamond.addEventListener('click', function() {
+    if (!isStarted) {
+        start.style.display = 'none';
+        count.style.display = 'block';
+        isStarted = true;
+        let time = 1;
         count.textContent = time;
-        if (time >= 4) {
-            clearInterval(timer);
-            start.style.display = 'block';
-            count.style.display = 'none';
-            time = 1;
-        }
-    }, 1000);
+        let timer = setInterval(() => {
+            time++;
+            count.textContent = time;
+            if (time >= 4) {
+                clearInterval(timer);
+                start.style.display = 'block';
+                count.style.display = 'none';
+                time = 1;
+                isStarted = false;
+            }
+        }, 1000);
+    } 
 });
-diamond.addEventListener('mouseenter', () => {
+diamond.addEventListener('mouseenter', function() {
     if (isStarted) body.style.background = 'rgb(191, 116, 241)';   
 });
-diamond.addEventListener('mouseleave', () => {
+diamond.addEventListener('mouseleave', function() {
     body.style.background = 'white';
 });
